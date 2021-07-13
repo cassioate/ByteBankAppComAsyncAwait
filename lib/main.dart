2134b5216b2cb7708a84d06aaee8a79f2202a5ac
Cassio <cassioate@gmail.com>
2021-07-13 03:07:26 -0300

    limpando base de dados local com onDowngrade: onDatabaseDowngradeDelete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:bytebank/Model/Contact.dart';
 import 'package:bytebank/Widgets/BytebankApp.dart';
 import 'package:flutter/material.dart';
-
-import 'database/app_database.dart';
 
 void main() {
   runApp(BytebankApp());
